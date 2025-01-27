5a4d8ee209fc30dbc984e27412277870147a54a3
Kamal <kamaldeep734833@gmail.com>
2025-01-27 20:21:12 +0530

    add a endpoint in QuizController

--- src/main/java/com/kamal/quizapp/controller/QuizController.java
+++ src/main/java/com/kamal/quizapp/controller/QuizController.java
@@ -1,19 +1,32 @@
 package com.kamal.quizapp.controller;
 
 import com.kamal.quizapp.model.Question;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("quiz")
+@RequestMapping("/quiz")
 public class QuizController {
 
     @GetMapping
     public ResponseEntity<List<Question>> quizQuestion(){
         return null;
+    }
+
+    @GetMapping("/start")
+    public ResponseEntity<?> getQuiz(@RequestParam(required = false) String category,
+                                     @RequestParam(required = false) Long noOfQuestion,
+                                     @RequestParam(required = false) String title){
+        System.out.println("all values:");
+        System.out.println("Category: "+category);
+        System.out.println("NoOfQuestion: "+noOfQuestion);
+        System.out.println("Title: "+title);
+        return new ResponseEntity<>("It's working perfectly",HttpStatus.OK);
     }
 }
